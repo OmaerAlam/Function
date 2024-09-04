bbd445feaa8cf85904ba3ea3cc6caebbc411cf88
OmaerAlam <114277920+OmaerAlam@users.noreply.github.com>
2024-09-04 12:01:11 +0600

    Add files via upload

---
+++ F5.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void swap(int &a, int &b){
+    a=300;
+    b=700;
+
+    return;
+
+}
+
+int main(){
+    int c=10, d=20;
+    swap(c,d);
+    cout<<" c = "<<c<<" d = "<<d<<endl;
+
+return 0;
+}
+
+
+
